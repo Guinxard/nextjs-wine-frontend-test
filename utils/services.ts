cb3a5ab8b4d775200373e1f411cfe90d13c18b7e
Guinxard <lucasmfarias3@gmail.com>
2022-07-01 00:05:17 -0300

    feat: creates fetchProducts, Product and home page props interfaces

---
+++ utils/services.ts
@@ -0,0 +1,8 @@
+ 
+export const fetchProducts = async(requestOptions:RequestInit) => {
+  const response = await fetch
+  ("https://wine-back-test.herokuapp.com/products", requestOptions);
+  const data = await response.json();
+  const products = data.items;
+  return products
+  }
